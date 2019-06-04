fefee67008c1775d1495b0b9833be110ebc3d002
shen_wen_hui <shenwenhui@zuzuche.com>
2019-06-04 12:27:02 +0800

    update

--- src/Ajaxray/PHPWatermark/import.php
+++ src/Ajaxray/PHPWatermark/import.php
@@ -9,3 +9,4 @@ include_once __DIR__.'/Requirements/RequirementsChecker.php';
 include_once __DIR__.'/Watermark.php';
 
 //外部调用设置命令前缀：\Ajaxray\PHPWatermark\Watermark::$commandPrefix='"C:\\Program Files\\ImageMagick-7.0.8-Q16\\magick" ';
+//setlocale(LC_CTYPE, "en_US.UTF-8");
